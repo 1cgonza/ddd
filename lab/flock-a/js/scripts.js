972bccdc604455b56cfc4e339e2871cd626d960e
Juan Camilo Gonzalez <info@juancgonzalez.com>
2015-09-16 18:57:11 +0800

    Update 2015-09-16T10:57:06.125Z

--- lab/flock-a/js/scripts.js
+++ lab/flock-a/js/scripts.js
@@ -1,3 +1,4 @@
+flag = 0;
 (function () {
   'use strict';
 
@@ -13,19 +14,32 @@
   var dataI = 0;
   var num = 1555; // 1555 victims from 1988-2012
   var vLimit = 10;
-  var currentYear = 2003;
   var animReq;
   var mode = -1;
   var currentX = 0;
   var currentY = 0;
   var TWO_PI = Math.PI * 2;
+  var tick = 0;
 
   // Sprite
   var img = new Image();
+  var imgLoaded = false;
+  var cols = 14;
+  var rows = 1;
+  var imgW = 614;
+  var imgH = 50;
+  var offX = 25;
+  var offY = 25;
+  var frameW = Math.round(imgW / cols);
+  var frameH = Math.round(imgH / rows);
 
   // MAP
   var zoom = 13;
   var mapCenter = convertCoordinates(-71.999996, 4.000002);
+
+  // MENU
+  var current;
+  var currentYear;
 
   /*----------  CREATE CANVAS  ----------*/
   var canvas = document.createElement('canvas');
@@ -34,7 +48,29 @@
   canvas.height = stageH;
   container.appendChild(canvas);
 
-  requestData('../../data/ingeominas/eq' + currentYear + '.json', processEQData);
+  yearsListMenu (1993, 2015, 2003, yearClickEvent, menuReady);
+
+  function menuReady (menu, currentBtn) {
+    container.appendChild(menu);
+    current = currentBtn;
+    currentYear = currentBtn.textContent;
+    init();
+  }
+
+  function yearClickEvent (event) {
+    window.cancelAnimationFrame(animReq);
+    ctx.clearRect(0, 0, stageW, stageH);
+    currentYear = event.target.textContent;
+
+    resetCurrentClass(current, event.target);
+    current = event.target;
+
+    init();
+  }
+
+  function init() {
+    requestData('../../data/ingeominas/eq' + currentYear + '.json', processEQData);
+  }
 
   function convertCoordinates (lon, lat) {
     var zoomX = stageW * zoom;
@@ -56,13 +92,15 @@
     eqData = data;
 
     for (var i = 0; i < num; i++) {
-      var x = getRandom(0, stageW);
+      var x = getRandom(-stageH, 0);
       var y = getRandom(0, stageH);
       flock[i] = new Bird(x, y);
     }
 
-    img.onload = imageReady;
-    img.src = '../../img/sprites/birdFly_f5_w136_h30.png';
+    if (!imgLoaded) {
+      img.onload = imageReady;
+      img.src = '../../img/sprites/bird2.png';
+    }
   }
 
   function imageReady () {
@@ -78,13 +116,20 @@
         var coords = convertCoordinates(d.lon, d.lat);
         currentX = coords.x + centerX;
         currentY = coords.y + centerY;
-      }
-      ctx.clearRect(0, 0, stageW, stageH);
 
-      for (var i = 0; i < flock.length; i++) {
-        flock[i].draw(currentX, currentY);
       }
-      ctx.fillRect(currentX, currentY, 5, 5);
+
+      if (tick > 3) {
+        var orientationFix = mode === 1 ? Math.PI : 0;
+        ctx.clearRect(0, 0, stageW, stageH);
+        for (var i = 0; i < flock.length; i++) {
+          flock[i].update(currentX, currentY);
+          flock[i].draw(orientationFix);
+        }
+        tick = 0;
+        ctx.fillRect(currentX, currentY, 5, 5);
+      }
+      tick++;
 
       dataI++;
       animReq = requestAnimationFrame(animate);
@@ -100,17 +145,16 @@
     this.vy = 0;
     this.ax = 0;
     this.ay = 0;
-    this.mass = getRandom(10, 20);
+    this.mass = getRandom(20, 30);
     this.orientation = 0;
-    this.turnSpeed = TWO_PI / 100;
-    this.frameX = getRandom(0, 5);
-    this.counter = 0;
+    this.turnSpeed = TWO_PI / 30;
+    this.frameX = getRandom(0, 13);
   }
 
-  Bird.prototype.draw = function(tx, ty) {
+  Bird.prototype.update = function(tx, ty) {
     var dx = this.x - tx;
     var dy = this.y - ty;
-    var distance = Math.pow(dx, 2) + Math.pow(dy, 2);
+    // var distance = Math.pow(dx, 2) + Math.pow(dy, 2);
     var theta = Math.atan2(dy, dx);
     var delta = theta - this.orientation;
     var deltaAbs = Math.abs(delta);
@@ -126,35 +170,30 @@
 
     this.orientation %= TWO_PI;
 
-    this.ax = this.mass * mode;
+    this.ax = this.mass * mode + 0.05;
     this.ay = (this.mass / 3) * mode;
     this.x += Math.cos(this.orientation) * this.ax;
     this.y += Math.sin(this.orientation) * this.ay;
 
-    if (this.counter < 4) {
-      if (theta > 1 ) this.frameX++;
-      if (this.frameX >= 5) {
-        this.frameX = 0;
-      }
-    } else {
-      this.counter = 0;
+    this.frameX++;
+
+    if (this.frameX >= 14) {
+      this.frameX = 0;
     }
-    this.counter++;
+  };
 
-
+  Bird.prototype.draw = function(r) {
     ctx.save();
       ctx.translate(this.x, this.y);
-      ctx.rotate(this.orientation);
+      ctx.rotate(this.orientation - r);
       ctx.drawImage(
         img,
-        this.frameX * 27, 0,
-        27, 30,
-        0, 0,
-        27, 30
+        this.frameX * frameW, 0,
+        frameW, frameH,
+        -offX, -offY,
+        frameW, frameH
       );
-
     ctx.restore();
-
   };
 
 })();
